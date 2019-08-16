bee55f7dc425b96ba37495ca4c6c05c40c344f79
Bryan M. Karpowicz <bryan.m.karpowicz@nasa.gov>
2019-08-16 14:02:42 -0400

    add new interface to test cases.

--- testCases/test_cris_no_clouds.py
+++ testCases/test_cris_no_clouds.py
@@ -1,78 +1,108 @@
 #!/usr/bin/env python3
 import configparser
-import os, h5py,sys 
+import os, h5py, sys 
 import numpy as np
 from matplotlib import pyplot as plt
 thisDir = os.path.dirname(os.path.abspath(__file__))
 parentDir = os.path.dirname(thisDir)
 sys.path.insert(0,parentDir)
-from pycrtm import pycrtm
-
+from pyCRTM import pyCRTM, profilesCreate
+ 
 def main(coefficientPath, sensor_id):
-    salinity = 33.0
     thisDir = os.path.dirname(os.path.abspath(__file__))
-    cases = os.listdir( os.path.join(thisDir,'data') ) 
+    cases = os.listdir( os.path.join(thisDir,'data') )
     cases.sort()
-    for c in cases:
-        h5 = h5py.File(os.path.join(thisDir, 'data', c) , 'r')
-        nChan = np.asarray(h5['Tb']).shape[0] 
-        nans = -9999.9* np.ones(np.asarray(h5['aerosolConcentration']).shape)
+    # create 4 profiles for each of the 4 cases
+    profiles = profilesCreate( 4, 92 )
+    storedTb = []
+    storedEmis = []
+    # populate the cases, and previously calculated Tb from crtm test program.    
+    for i,c in enumerate(cases):
+        h5 = h5py.File(os.path.join(thisDir,'data',c) , 'r')
+        profiles.Angles[i,0] = h5['zenithAngle'][()]
+        profiles.Angles[i,1] = 999.9 
+        profiles.Angles[i,2] = 100.0  # 100 degrees zenith below horizon.
+        profiles.Angles[i,3] = 0.0 # zero solar azimuth 
+        profiles.Angles[i,4] = h5['scanAngle'][()]
+        profiles.DateTimes[i,0] = 2001
+        profiles.DateTimes[i,1] = 1
+        profiles.DateTimes[i,2] = 1
+        profiles.Pi[i,:] = np.asarray(h5['pressureLevels'] )
+        profiles.P[i,:] = np.asarray(h5['pressureLayers'][()])
+        profiles.T[i,:] = np.asarray(h5['temperatureLayers'])
+        profiles.Q[i,:] = np.asarray(h5['humidityLayers'])
+        profiles.O3[i,:] = np.asarray(h5['ozoneConcLayers'])
+        profiles.CO2[i,:] = np.asarray(h5['co2ConcLayers'])
+        profiles.clouds[i,:,0,0] = np.asarray(h5['cloudConcentration'])
+        profiles.clouds[i,:,0,1] = np.asarray(h5['cloudEffectiveRadius'])
+        profiles.aerosols[i,:,0,0] = np.asarray(h5['aerosolConcentration'])
+        profiles.aerosols[i,:,0,1] = np.asarray(h5['aerosolEffectiveRadius'])
+        profiles.aerosolType[i] = -1 #h5['aerosolType'][()]
+        profiles.cloudType[i] = -1  #h5['cloudType'][()]
+        profiles.cloudFraction[i,:] = h5['cloudFraction'][()]
+        profiles.climatology[i] = h5['climatology'][()]
+        profiles.surfaceFractions[i,:] = h5['surfaceFractions']
+        profiles.surfaceTemperatures[i,:] = h5['surfaceTemperatures']
+        profiles.S2m[i,1] = 33.0 # just use salinity out of S2m for the moment.
+        profiles.windSpeed10m[i] = 5.0
+        profiles.LAI[i] = h5['LAI'][()]
+        profiles.windDirection10m[i] = h5['windDirection10m'][()]
+        profiles.n_absorbers[i] = h5['n_absorbers'][()]
+        # land, soil, veg, water, snow, ice
+        profiles.surfaceTypes[i,0] = h5['landType'][()]
+        profiles.surfaceTypes[i,1] = h5['soilType'][()]
+        profiles.surfaceTypes[i,2] = h5['vegType'][()]
+        profiles.surfaceTypes[i,3] = h5['waterType'][()]
+        profiles.surfaceTypes[i,4] = h5['snowType'][()]
+        profiles.surfaceTypes[i,5] = h5['iceType'][()]
+        storedTb.append(np.asarray(h5['Tb']))
+        storedEmis.append(np.asarray(h5['emissivity_atms']))
+        h5.close()
 
-        forwardTb, forwardTransmission,\
-        forwardEmissivity = pycrtm.wrap_forward( coefficientPath, sensor_id,\
-                        h5['zenithAngle'][()], h5['scanAngle'][()], 999.9, np.asarray([100.0,0.0]).reshape([1,2]),2001,1,1, nChan, \
-                        np.asarray(h5['pressureLevels']).reshape([1,93]),np.asarray(h5['pressureLayers']).reshape([1,92]), np.asarray(h5['temperatureLayers']).reshape([1,92]), np.asarray(h5['humidityLayers']).reshape([1,92]), np.asarray(h5['ozoneConcLayers']).reshape([1,92]),\
-                        np.asarray(h5['co2ConcLayers']).reshape([1,92]),\
-                        np.asarray(h5['aerosolEffectiveRadius']).reshape([1,92,1]), np.asarray(h5['aerosolConcentration']).reshape([1,92,1]), -1, \
-                        np.asarray(h5['cloudEffectiveRadius']).reshape([1,92,1]), np.asarray(h5['cloudConcentration']).reshape([1,92,1]), -1, np.asarray(h5['cloudFraction']).reshape([1,92]), h5['climatology'][()], \
-                        np.asarray(h5['surfaceTemperatures']).reshape([1,4]), np.asarray(h5['surfaceFractions']).reshape([1,4]), h5['LAI'][()], salinity, np.float(5.0), h5['windDirection10m'][()], h5['n_absorbers'][()],\
-                        h5['landType'][()], h5['soilType'][()], h5['vegType'][()], h5['waterType'][()], h5['snowType'][()], h5['iceType'][()], 1 )
+    crtmOb = pyCRTM()
+    crtmOb.profiles = profiles
+    crtmOb.coefficientPath = pathInfo['CRTM']['coeffs_dir']
+    crtmOb.sensor_id = sensor_id
+    crtmOb.nThreads = 4
 
-        kTb, kTransmission,\
-        temperatureJacobian,\
-        humidityJacobian,\
-        ozoneJacobian, kEmissivity = pycrtm.wrap_k_matrix( coefficientPath, sensor_id,\
-                        h5['zenithAngle'][()], h5['scanAngle'][()], 999.9, np.asarray([100.0,0.0]).reshape([1,2]), 2001,1,1, nChan, \
-                        np.asarray(h5['pressureLevels']).reshape([1,93]),np.asarray(h5['pressureLayers']).reshape([1,92]), np.asarray(h5['temperatureLayers']).reshape([1,92]), np.asarray(h5['humidityLayers']).reshape([1,92]), np.asarray(h5['ozoneConcLayers']).reshape([1,92]),\
-                        np.asarray(h5['co2ConcLayers']).reshape([1,92]),\
-                        np.asarray(h5['aerosolEffectiveRadius']).reshape([1,92,1]), np.asarray(h5['aerosolConcentration']).reshape([1,92,1]), -1, \
-                        np.asarray(h5['cloudEffectiveRadius']).reshape([1,92,1]), np.asarray(h5['cloudConcentration']).reshape([1,92,1]), -1, np.asarray(h5['cloudFraction']).reshape([1,92]), h5['climatology'][()], \
-                        np.asarray(h5['surfaceTemperatures']).reshape([1,4]), np.asarray(h5['surfaceFractions']).reshape([1,4]), h5['LAI'][()], salinity, np.float(5.0), h5['windDirection10m'][()], h5['n_absorbers'][()],\
-                        h5['landType'][()], h5['soilType'][()], h5['vegType'][()], h5['waterType'][()], h5['snowType'][()], h5['iceType'][()], 1 )
+    crtmOb.loadInst()
 
+    crtmOb.runDirect()
+    forwardTb = crtmOb.Bt
+    forwardEmissivity = crtmOb.surfEmisRefl
+
+    crtmOb.runK()
+    kTb = crtmOb.Bt
+    kEmissivity = crtmOb.surfEmisRefl
+
+    if ( all( np.abs( forwardTb.flatten() - np.asarray(storedTb).flatten() ) <= 1e-5)  and all( np.abs( kTb.flatten() - np.asarray(storedTb).flatten() ) <= 1e-5) ):
+        print("Yay! all values are close enough to what CRTM test program produced!")
+    else: 
+        print("Boo! something failed. Look at cris plots")
+        wavenumbers = np.zeros([4,1305])
+        wavenumbers[0:4,:] = np.linspace(1,1306,1305)
         
-        diffK = kTb.flatten()-h5['Tb']
-        diffKemis = kEmissivity.flatten()-h5['emissivity']
-        
-        diff = forwardTb.flatten()-h5['Tb']
-        diffEmis = forwardEmissivity.flatten()-h5['emissivity']
-        if ( all(np.abs(diffKemis) <= 1e-8)  and all(np.abs(diffK) <= 1e-8) and  all(np.abs(diffEmis) <= 1e-8)  and all(np.abs(diff) <= 1e-8)  ):
-            print ("Yay! we duplicated results from CRTM test program!")
-        else:
-            h5wav = h5py.File(os.path.join(thisDir,'cris_wavenumbers.h5'),'r')
-            wavenumbers = np.asarray(h5wav['wavenumbers'])
-            
-            plt.figure()
-            plt.plot(wavenumbers,diffK)
-            plt.savefig( os.path.join(thisDir,c+'_spectrum_k_matrix.png') )
+        plt.figure()
+        plt.plot(wavenumbers.T,forwardTb.T-np.asarray(storedTb).T ) 
+        plt.legend(['1','2','3','4'])
+        plt.savefig(os.path.join(thisDir,'cris'+'_spectrum_forward.png'))
+        plt.figure()
+        plt.plot(wavenumbers.T,forwardEmissivity.T-np.asarray(storedEmis).T)
+        plt.savefig(os.path.join(thisDir,'cris'+'_emissivity_forward.png')) 
+    
+        plt.figure()
+        plt.plot(wavenumbers.T,kTb.T-np.asarray(storedTb).T)
+        plt.savefig(os.path.join(thisDir,'cris'+'_spectrum_k.png'))
+        plt.figure()
+        plt.plot(wavenumbers.T,kEmissivity.T-np.asarray(storedEmis).T)
+        plt.savefig(os.path.join(thisDir,'cris'+'_emissivity_k.png')) 
+        sys.exit("Boo! didn't pass tolerance with CRTM test program.")
 
-            plt.figure()
-            plt.plot(wavenumbers,diffKemis)
-            plt.savefig( os.path.join(thisDir,c+'_emissivity_k_matrix.png') ) 
-
-            plt.figure()
-            plt.plot(wavenumbers,forwardTb.flatten()-h5['Tb'])
-            plt.savefig( os.path.join(thisDir,c+'_spectrum_forward.png') )
-
-            plt.figure()
-            plt.plot(wavenumbers,forwardEmissivity.flatten()-h5['emissivity'])
-            plt.savefig( os.path.join(thisDir,c+'_emissivity_forward.png') ) 
-            print("Boo! {} failed to pass a test. look at plots for details in {}.".format(c,thisDir))
 
 if __name__ == "__main__":
     pathInfo = configparser.ConfigParser()
-    pathInfo.read( os.path.join( parentDir, 'crtm.cfg' ) )
+    pathInfo.read( os.path.join(parentDir,'crtm.cfg') ) 
     coefficientPath = pathInfo['CRTM']['coeffs_dir']
-    sensor_id = 'cris_npp' 
+    sensor_id = 'cris_npp'
     main(coefficientPath, sensor_id)
  
